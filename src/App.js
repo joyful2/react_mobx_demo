c4d8a0614fdc8cc93d0fb48ba5c9423871821a6b
v_chunxyuan <v_chunxyuan@tencent.com>
2020-08-19 20:15:57 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {
+	observable
+} from 'mobx'
+
+import {
+	observer
+} from 'mobx-react'
+
+@observer
+class App extends Component {
+	@observable value = 'a'
+	constructor(props) {
+		super(props)
+		this.state = {
+			time: 0
+		}
+	}
+
+	render() {
+		const {
+			time
+		} = this.state
+		return (
+			<div>
+				app
+				<input value={this.value} onChange={e => this.value = e.target.value} />
+				{this.value}
+			</div>
+		);
+	}
+
+}
+
+export default App;
